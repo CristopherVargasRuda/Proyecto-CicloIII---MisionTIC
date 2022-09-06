0883597fd787a6ceed94f4b39c540a485be20669
mangel6 <lozanom521@gmail.com>
2022-09-05 21:09:13 -0500

    Frontend/ Primera parte Consultar Impresoras

--- Impresoras3D.App/Impresoras3D.App.Frontend/Pages/Consultas/ConsultarImpresoras.cshtml.cs
+++ Impresoras3D.App/Impresoras3D.App.Frontend/Pages/Consultas/ConsultarImpresoras.cshtml.cs
@@ -11,20 +11,60 @@ namespace Impresoras3D.App.Frontend.Pages
             new Impresoras3D.App.Persistencia.AppContext()
         );
 
+        private static IRepositorioSoftware _repositorioSoftware = new RepositorioSoftware(
+            new Impresoras3D.App.Persistencia.AppContext()
+        );
+
+        private static IRepositorioEstado _repositorioEstado = new RepositorioEstado(
+            new Impresoras3D.App.Persistencia.AppContext()
+        );
+
+        private static IRepositorioComponente _repositorioComponente = new RepositorioComponente(
+            new Impresoras3D.App.Persistencia.AppContext()
+        );
+
+
+
         [BindProperty]
         public Impresora Impresora { get; set; }
+
+        public Impresora impresoraObtenida { get; set; }
+
+        public Software softwareObtenido { get; set; }
+
+        public Estado estadoSoftwareObtenido { get; set; }
+
+        public Estado estadoImpresoraObtenida { get; set; }
+
 
         public ConsultarImpresorasModel() { }
 
         public ActionResult OnGet()
         {
-            this.Impresora = null;
             return Page();
         }
 
-        public ActionResult OnPost() {
-            Impresora impresoraObtenida = _repositorioImpresora.getImpresora(Int32.Parse(Impresora.PlacaInventario));
+        public ActionResult OnPost()
+        {
+            try
+            {
+                this.impresoraObtenida = _repositorioImpresora.getByPlaca(
+                    Impresora.PlacaInventario
+                );
+
+                this.softwareObtenido = _repositorioSoftware.getSoftware(this.impresoraObtenida.SoftwareId); //REVISAR!
+
+                this. estadoImpresoraObtenida = _repositorioEstado.getEstado(3); //REVISAR!!
+
+                this.estadoSoftwareObtenido = _repositorioEstado.getEstado(8); //REVISAR!!
+            }
+            catch (System.Exception e)
+            {
+                ViewData["Error"] = e.Message;
+                Console.Out.WriteLine(Impresora.PlacaInventario);
+            }
+
             return Page();
-         }
+        }
     }
 }
