85c36948dfa6c66b8cb41aced72179c83f41fb6f
unknown <cristophervargasruda@gmail.com>
2022-09-11 11:36:39 -0500

    Creación de tablas de revisiones

--- Impresoras3D.App/Impresoras3D.App.Frontend/Pages/Consultas/ConsultarRevisiones.cshtml.cs
+++ Impresoras3D.App/Impresoras3D.App.Frontend/Pages/Consultas/ConsultarRevisiones.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Impresoras3D.App.Dominio;
@@ -8,10 +9,20 @@ namespace Impresoras3D.App.Frontend.Pages
     public class ConsultarRevisionesModel : PageModel
     {
         private static IRepositorioImpresora _repositorioImpresora = new RepositorioImpresora(new Impresoras3D.App.Persistencia.AppContext());
+        private static IRepositorioServicioTecnico _repositorioServicioTecnico = new RepositorioServicioTecnico(new Impresoras3D.App.Persistencia.AppContext());
+        private static IRepositorioTecnico _repositorioTecnico = new RepositorioTecnico(new Impresoras3D.App.Persistencia.AppContext());
         [BindProperty]
         public Impresora Impresora { get; set; }
-        public void OnGet()
+        [BindProperty]
+        public IEnumerable<ServicioTecnico> ServiciosTecnicos { get; set; }
+        [BindProperty]
+        public IEnumerable<Tecnico> Tecnicos { get; set; }
+        public ActionResult OnGet(int id)
         {
+            Impresora = _repositorioImpresora.getImpresora(id);
+            Tecnicos = _repositorioTecnico.GetAllTecnico();
+            ServiciosTecnicos = _repositorioServicioTecnico.GetServiciosTecnicosByImpresoraId(id);
+            return Page();
         }
     }
 }
